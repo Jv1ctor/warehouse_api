89d395c423c77cdfc6ee9433fd08ace94e27183f
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-06-14 22:26:28 -0300

    add auth middleware in routes

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 
 import { authRoutes } from './modules/auth/auth.router';
+import { authMiddleware } from './modules/auth/middleware/auth.middleware';
+import { userRepository } from './modules/auth/user.repository';
 import { categoriesRoutes } from './modules/categories/categories.router';
 import { productRoutes } from './modules/product/product.router';
 import { stockMovementRoutes } from './modules/stock-movement/stock-movement.router';
@@ -8,8 +10,10 @@ import { supplierRoutes } from './modules/supplier/supplier.router';
 
 export const router = Router();
 
-router.use('/auth', authRoutes);
-router.use('/categories', categoriesRoutes);
-router.use('/products', productRoutes);
-router.use('/stock-movements', stockMovementRoutes);
-router.use('/suppliers', supplierRoutes);
+const authenticator = authMiddleware({ repository: userRepository });
+
+router.use('/auth', authenticator, authRoutes);
+router.use('/categories', authenticator, categoriesRoutes);
+router.use('/products', authenticator, productRoutes);
+router.use('/stock-movements', authenticator, stockMovementRoutes);
+router.use('/suppliers', authenticator, supplierRoutes);
