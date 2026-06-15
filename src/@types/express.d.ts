c1c8443804cfede9878f1fcd29647efeb97fea59
Jv1ctor <j.rodriguesbarretos@gmail.com>
2026-06-14 22:20:11 -0300

    refactor(express-types): adjust request extra typing

--- src/@types/express.d.ts
+++ src/@types/express.d.ts
@@ -1,7 +1,19 @@
+import type { Request as ExpressRequest } from 'express';
+
 declare global {
   namespace Express {
     interface Request {
-      user?: object;
+      user?: { id: number; name: string };
     }
   }
 }
+
+declare module 'express' {
+  export interface Request extends ExpressRequest {
+    user?: { id: number; name: string };
+  }
+}
+
+export type AuthRequest = ExpressRequest & {
+  user?: { id: number; name: string };
+};
